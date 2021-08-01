53233b9c52a39a2506c3dd87fa01bd872ea39f5a
unknown <mzshaikh129@gmail.com>
2021-08-01 12:50:11 +0500

    initial remote commit

--- lib/Screens/TransactionScreen.dart
+++ lib/Screens/TransactionScreen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 class TransactionScreen extends StatelessWidget {
-  const TransactionScreen({ Key? key }) : super(key: key);
+  const TransactionScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Transaction History")),
+      ),
+      body: DefaultTabController(
+        length: 2,
+        child: TabBar(
+          tabs: [Text("Completed Payouts"), Text("Upcoming Payouts")],
+        ),
+      ),
     );
   }
 }
